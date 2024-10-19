da28b37b79b99dc83828ef6d90a5459d99a82882
Navabharathi <127295680+nb242003@users.noreply.github.com>
2024-10-19 19:16:26 +0530

    Update About.tsx

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import React from 'react';
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
 import book from "../assets/book.png";
 import pc from "../assets/pc.png";
 import card from "../assets/card.png";
